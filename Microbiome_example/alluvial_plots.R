45c408b12a3fd6f776badac13d4e198b1645c1a2
Ricardo I <ralcala@ufl.edu>
2021-02-17 19:12:58 -0500

    Update alluvial_plots.R

--- Microbiome_example/alluvial_plots.R
+++ Microbiome_example/alluvial_plots.R
@@ -1,71 +1,44 @@
 # R Alcala 
-#     Papaya metagenomics   
 #       Network analysis      
-#         5 Generating alluvial plots
+#       Generating alluvial plots
 
+# load libraries
+library(alluvial)
+library(tidyverse)
+library(reshape2)
 
-#dat <- read.table(text = "A   B   C   D   E   F    G
-#1 480 780 431 295 670 360  190
-#                  2 720 350 377 255 340 615  345
-#                  3 460 480 179 560  60 735 1260
-#                  4 220 240 876 789 820 100   75", header = TRUE)
-#dat$row <- seq_len(nrow(dat))
-#dat2 <- melt(dat, id.vars = "row")
+# creating a data.frame as an example
+dat <- read.table(text = "
+  Phylum     Class    Order        Genus      Continent abundance
+  Chordata   Mammalia Artiodactyla Cetacea    worldwide 10
+  Chordata   Mammalia Carnivora    Panther    Asia      45
+  Chordata   Tunicata NA           Ascidiacea worldwide 6
+  Chordata   Reptilia Squamata     Afrogecko  Africa    6
+  Arthropoda Insecta  Diptera     Drosophila  worlwide  20", 
+ header = TRUE)
 
-#Family
-datc <- RPapaya %>%
-  select(Family, Genus, OTU, length, cov)
-#x = a + (X - A) * (b - a) / (B - A)
-a = 1
-b = 0.1
-A <- max(datc$cov)
-B <- min(datc$cov)
-X <- datc$cov
-cov.norm <- a + (X - A) * (b - a) / (B - A)
-datc$cov.norm <- -log(cov.norm)
+# subsetting table
+datc <- dat %>%
+  select(Phylum, Class, Genus, Continent, abundance)
 head(datc)
-datc <- datc[which(datc$length >= 1000), ]
-#rainbow(length(unique(datc$Family)))
-RbPal <- rainbow(c(length(unique(datc$Family))+2))
-datc %>%
-  mutate(
-    ss = paste(Family, Genus),
-    k = RbPal[ match(ss, sort(unique(ss))) ]
-  ) -> datc
 
+# adding colors
+RbPal <- rainbow(c(length(unique(datc$Phylum))+2))
 
-alluvial(datc[,c(1,2,3)], freq=datc$cov.norm,
+datc <- datc %>%
+            mutate( ss = paste(Phylum, Class),
+                    cols = RbPal[ match(ss, sort(unique(ss))) ] 
+                    )
+# alluvla plot 
+
+alluvial(datc[,c(1,2,4)], freq=datc$abundance,
          #hide = datc$length == 0,
-         col = datc$k,
-         border = datc$k, 
-         alpha=0.7,
-         blocks=FALSE,
-         ordering = list( NULL, NULL, order(as.factor(datc$Family))), 
+         col = datc$cols,
+         border = datc$cols, 
+         alpha = 0.7,
+         blocks = FALSE,
+         ordering = list(NULL, NULL, order(as.factor(datc$Phylum))), 
+                # change NULL to order them
          cex =0.8
-)
+        )
 
-
-#######
-
-tit <- data.frame(Titanic)
-pal <- c("red4", "lightskyblue4", "red", "lightskyblue")
-
-tit %>%
-  mutate(
-    ss = paste(Survived, Sex),
-    k = pal[ match(ss, sort(unique(ss))) ]
-  ) -> tit
-
-
-alluvial(tit[,c(2,3, 4)], freq=tit$Freq,
-         hide = tit$Freq < 10,
-         col = tit$k,
-         border = tit$k,
-         blocks=FALSE,
-         ordering = list(
-           NULL,
-           NULL,
-           order(tit$Age, tit$Sex )
-           
-         )
-)
